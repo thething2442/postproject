2f8dcc117125c69dfbc01aca37f0503f644d14e7
thething2442 <developmentserver160@gmail.com>
2025-09-24 14:31:02 +0000

    make the '/' public all to

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
-const isOnboardingRoute = createRouteMatcher(['/','/postproject'])
+const isOnboardingRoute = createRouteMatcher(['/postproject'])
 const isPublicRoute = createRouteMatcher(['/sign-up','sign-in','/about',])
 
 export default clerkMiddleware(async (auth, req) => {
