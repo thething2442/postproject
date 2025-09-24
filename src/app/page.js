f58e21498a012fc20e2f5d171fa19fa301d0538c
thething2442 <developmentserver160@gmail.com>
2025-09-24 17:36:22 +0000

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,9 @@
-
-import { LoginForm } from "@/src/app/components/login-form"
-import { WelcomeHeader } from "@/src/app/components/welcome-header"
-
+'use client'
+import { UserButton } from "@clerk/nextjs"
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-background dark">
-      <div className="container mx-auto px-4 py-8">
-        <div className="flex min-h-screen flex-col items-center justify-center">
-          <div className="w-full max-w-md space-y-8">
-            <WelcomeHeader />
-            <LoginForm />
-          </div>
-        </div>
-      </div>
+      <UserButton />
     </div>
   )
 }
