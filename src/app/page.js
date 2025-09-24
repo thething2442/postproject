974b36891df6a5197bc8b4030e85839d60e4bd37
thething2442 <developmentserver160@gmail.com>
2025-09-24 19:40:56 +0000

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
 'use client'
-import { UserButton } from "@clerk/nextjs"
+import { useUser, useClerk } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+import { Avatar, AvatarImage } from "./components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
+
 export default function LoginPage() {
+  const {user,isLoaded,isSignedIn} = useUser();
+  const { signOut } = useClerk();
+
+  if(!isLoaded){return <div>Loading...</div> }
+  if(!isSignedIn){redirect('/sign-up')}
   return (
-    <div className="min-h-screen bg-background dark">
-      <UserButton />
-    </div>
+    <div className="min-h-screen bg-background dark p-4 flex justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src={user.imageUrl} />
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => signOut({ redirectUrl: '/sign-in' })}>Sign Out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+    </div>  
   )
 }
